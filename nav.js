65f2941ca1ad71032a5cbea7bbd36a07586b151d
Cheryl Miyake <52255583+clmiyake@users.noreply.github.com>
2020-03-18 00:07:20 -0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,11 @@
+//nav scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-135px";
+        }
+    prevScrollpos = currentScrollPos;
+    }
